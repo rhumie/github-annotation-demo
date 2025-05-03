58c508a0cbf9f0c335b6c0c9db33ce23c392c5e6
Hiroki Takeda <takeda.hiroki.tech@gmail.com>
2025-05-03 12:04:37 +0900

    feat: create a sample frontend project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const log = (message: any) => {
-  console.log(message)
-}
-log('Debugging')
-
 createApp(App).mount('#app')
