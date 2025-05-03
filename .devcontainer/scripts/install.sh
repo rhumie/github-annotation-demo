d95518fa8295595c619af9ba6726afa86167d325
Hiroki Takeda <takeda.hiroki.tech@gmail.com>
2025-05-03 12:04:17 +0900

    chore: setup dev container

---
+++ .devcontainer/scripts/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Update System Packages.
+sudo apt-get update
+
+# Install gnupg2 for sharing Git credentials.
+# cf. https://code.visualstudio.com/remote/advancedcontainers/sharing-git-credentials#_sharing-gpg-keys
+sudo apt-get install -y gnupg2
+
+# Set the owner of the directories mounted via the volumes.
+sudo chown vscode:vscode frontend/node_modules
+
+# Set aliases
+echo "alias ll=\"ls -l\"" >> "$HOME/.bash_aliases"
